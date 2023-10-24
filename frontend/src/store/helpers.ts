95835134b358084e4053605d426b8adc79185fc9
Loren Grixti <loren@beyondplay.io>
2023-10-24 22:17:03 +0200

    Refactor Helper function

--- frontend/src/store/helpers.ts
+++ frontend/src/store/helpers.ts
@@ -1,42 +1,51 @@
-function formatDecimals(number: number) {
-    //If decimal value is [zero] then truncate it
-    //Otherwise format to 1 decimal place
-    return number % 1 === 0
-        ? number.toFixed(0)
-        : number.toFixed(1);
-}
-
 /**
  * Format a number with appropriate suffix and 1 decimal place.
  * @param number - The number to format.
  * @returns Formatted string with a suffix (T, B, M, K) and 1 decimal place.
  */
-export function formatLargeNumber(number: number) {
-    let result;
 
-    if (!number) return "";
+type FormatterReturn = (number: number) => string
 
-    switch (true) {
-        case number >= 1e12:
-            // If value is in trillions
-            result = formatDecimals((number / 1e12)) + 'T';
-            break;
-        case number >= 1e9:
-            // If value is in billions
-            result = formatDecimals((number / 1e9)) + 'B ';
-            break;
-        case number >= 1e6:
-            // If value is in millions
-            result = formatDecimals((number / 1e6)) + 'M';
-            break;
-        case number >= 1e3:
-            // If value is in thousands
-            result = formatDecimals((number / 1e3)) + 'K';
-            break;
-        default:
-            // If it's not in thousands, millions, billions, or trillions, return the number as is
-            result = number.toString();
+export function createFormatter(): FormatterReturn {
+    /**
+     * Format it to 1 decimal place.
+     * @param number - The number to format.
+     * @returns Formatted string with 1 decimal place.
+     */
+    const formatDecimals = (number: number, decimalPlaces: number = 0) => number % 1 === 0
+        ? number.toFixed(0)
+        : number.toFixed(decimalPlaces);
+
+    function formatLargeNumber(number: number): string {
+        let result;
+
+        if (!number) return "";
+        const trunc1Decimal = (number: number) => formatDecimals(number, 1);
+        const trunc2Decimal = (number: number) => formatDecimals(number, 2);
+
+        switch (true) {
+            case number >= 1e12:
+                // If value is in trillions
+                result = trunc1Decimal((number / 1e12)) + 'T';
+                break;
+            case number >= 1e9:
+                // If value is in billions
+                result = trunc1Decimal((number / 1e9)) + 'B ';
+                break;
+            case number >= 1e6:
+                // If value is in millions
+                result = trunc2Decimal((number / 1e6)) + 'M';
+                break;
+            case number >= 1e3:
+                // If value is in thousands
+                result = trunc2Decimal((number / 1e3)) + 'K';
+                break;
+            default:
+                // If it's not in thousands, millions, billions, or trillions, return the number as is
+                result = number.toString();
+        }
+        return result;
     }
-
-    return result;
+    return formatLargeNumber;
 }
+export const formatLargeNumber = createFormatter();
